700472f3b5ce53b3f023b6996d2a21045afb5b91
kannan <kannacbe@gmail.com>
2022-09-04 22:57:20 +0800

    changed pacakage

--- src/test/java/com/codechallenge/controller/InputCommandControllerTest.java
+++ src/test/java/com/codechallenge/controller/InputCommandControllerTest.java
@@ -1,4 +1,4 @@
-package com.jpmc.codechallenge.controller;
+package com.codechallenge.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
